795657cb18883699369eca2acdd9c7c4ecca72a6
aram-father <aram_father@naver.com>
2022-08-01 22:25:44 +0900

    [project1] Implement Dataset Split
    Implement split function to split the dataset into test, training,
    and validation sets.

--- create_splits.py
+++ create_splits.py
@@ -4,11 +4,12 @@ import os
 import random
 
 import numpy as np
+from collections import deque
 
 from utils import get_module_logger
 
 
-def split(source, destination):
+def split(source: str, destination: str):
     """
     Create three splits from the processed records. The files should be moved to new folders in the
     same directory. This folder should be named train, val and test.
@@ -17,7 +18,24 @@ def split(source, destination):
         - source [str]: source data directory, contains the processed tf records
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
-    # TODO: Implement function
+    records = glob.glob(f"{source}/*.tfrecord")
+    random.shuffle(records)
+    records = deque(records)
+
+    partition = {
+        "train": int(0.8 * len(records)),
+        "val": int(0.1 * len(records)),
+        "test": int(0.1 * len(records))
+    }
+
+    for pname in partition.keys():
+        os.mkdir(f"{destination}/{pname}")
+
+    for pname, psize in partition.items():
+        for it in range(psize):
+            src_path = records.popleft()
+            dst_path = f"{destination}/{pname}/{os.path.basename(src_path)}"
+            os.symlink(src_path, dst_path)
 
 
 if __name__ == "__main__":
